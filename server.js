7084a74f1b30ee230c7f442d087e16d7537ddbc8
MaxiCB <aaroncb@aaroncb.com>
2020-01-15 19:08:41 -0600

    Add express-session to project

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const session = require('express-session');
 
 const UserRoute = require("./users/user-route");
 
 const server = express();
 
 server.use(express.json());
+
+server.use(
+    session({
+        name: 'usersession',
+        secret: 'test secret',
+        cookie: {
+            maxAge: 1 * 24 * 60 * 60 * 1000,
+            secure: true
+        },
+        httpOnly: true,
+        resave: false,
+        saveUninitialized: false,
+    })
+)
 
 server.use('/api/users', UserRoute);
 server.use('/api/restricted', UserRoute);
